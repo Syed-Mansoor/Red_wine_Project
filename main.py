737527e40e421ebbef6cf7ddc3f0aa873aefaa99
Syed-mansoor <saeedmansoor56@gmail.com>
2024-08-26 12:23:49 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 from src.mlproject import logger
-logger.info('testing logging')
+from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
+
+STAGE_NAME = 'data ingestion stage'
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
+
+STAGE_NAME = 'data validation stage'
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/mlproject/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,33 @@
+# from src.mlproject.config.configuration import ConfigurationManager
+# from src.mlproject.components.data_ingestion import DataIngestion
+# from src.mlproject import logger
+
+from mlproject.config.configuration import ConfigurationManager
+from mlproject.components.data_ingestion import DataIngestion
+from mlproject import logger
+
+
+STAGE_NAME = 'data ingestion stage'
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        try:
+            config = ConfigurationManager()
+            data_ingestion_config = config.get_data_ingestion_config()
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+            data_ingestion.extract_zip_file()
+        except Exception as e:
+            raise e
+        
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
