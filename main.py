c93b94fe255e73d0d746871dc4bc486799b12cdd
Syed-mansoor <saeedmansoor56@gmail.com>
2024-08-26 13:08:32 +0530

    model training done

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.mlproject import logger
 from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.mlproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from pathlib import Path
 
 STAGE_NAME = 'data ingestion stage'
@@ -39,3 +40,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
